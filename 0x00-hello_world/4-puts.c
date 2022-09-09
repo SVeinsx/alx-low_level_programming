21e10634225b96c6a044becf85fe8002989daee1
Oluwanifemi Adewale <solarveins77@gmail.com>
2022-09-08 21:58:12 -0700

    Task 4-8 level C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
+ * /
+int main(void)
+{
+     puts("\"Programming is like building a multi lingual puzzle");
+     return (0)
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  main - Print a text
+ *
+ *  Return 0 (Successful)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0)
+}
